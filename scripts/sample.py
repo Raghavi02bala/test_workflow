215e90eabe862bc3990a03065976df7d7686ccb2
RAGHAVI B <68910030+Raghavi02bala@users.noreply.github.com>
2024-11-08 12:21:43 +0530

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,1 +1,9 @@
+import requests 
+import os
 
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def get_access_token():
+  print("Getting access token", client_id, client_secret)
+  return client_id, client_secret
