c840a0f16daeae3f23ca86185259dc15ac64eb01
RAGHAVI B <68910030+Raghavi02bala@users.noreply.github.com>
2024-11-08 16:06:27 +0530

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -3,6 +3,9 @@ import os
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+initiative_id = os.getenv("INITIATIVE_ID")
+team_name = os.getenv("TEAM_NAME")
+env = os.getenv("ENVIRONMENT")
 
 def create_access_token():
   url = "https://dev.auth.mlops.ingka.com/realms/istio/protocol/openid-connect/token"
@@ -12,7 +15,23 @@ def create_access_token():
   }
   
   response = requests.request("POST", url, headers=headers, data=payload)
-  return response
+  return response.json()['access_token']
 
-print(create_access_token())
+access_token = create_access_token()
 
+def create_client():
+  url = "https://dev.auth.mlops.ingka.com/admin/realms/istio/clients"
+  payload = json.dumps({
+    "clientId": f"client-{initiative_id}",
+    "rootUrl": f"http://{initiative_id}.{team_name}.{env}.mlops.ingka.com",
+    "redirectUris": [
+      f"http://{initiative_id}.{team_name}.{env}.mlops.ingka.com"
+    ]
+  })
+  headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {access_token}'
+  }
+
+  response = requests.request("POST", url, headers=headers, data=payload)
+  return response.json()
