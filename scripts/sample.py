4753be863e74dc0fe9fa53de80ad6130a1d435f8
RAGHAVI B <68910030+Raghavi02bala@users.noreply.github.com>
2024-11-08 13:35:35 +0530

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -4,6 +4,14 @@ import os
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-def get_access_token():
-  print("Getting access token", client_id, client_secret)
-  return client_id, client_secret
+def create_access_token():
+  url = "https://dev.auth.mlops.ingka.com/realms/istio/protocol/openid-connect/token"
+  payload = f'client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials'
+  headers = {
+  'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  response = requests.request("POST", url, headers=headers, data=payload)
+  return response
+
+
